63e9bf9ceeffb5d3fb327f3182623817beaf8017
chibashu <rogerkensny854@gmail.com>
2022-02-10 14:51:32 +0900

    フォームオブジェクトの実装

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,23 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!, except: :index
+
+  def index
+    @order_address = OrderAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+end
